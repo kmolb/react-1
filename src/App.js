196146e75893a829420ab83710300126bd99313a
Kamil Olber <olb.kamil@gmail.com>
2018-12-01 12:56:57 +0100

    cos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React, {
-  Component} from 'react';
+  Component
+} from 'react';
 import './App.css';
 
-function AppF(props){
-  return <div>Siemaaa</div>
-}
 
-class App extends Component{
-  render (){
-    return <div>Siemaaa</div>;
+const list = [{
+    name: "Wojtek",
+    surname: "Nowak",
+    age: 23,
+    id: 1
+  },
+
+  {
+    name: "Daniel",
+    surname: "Nowak",
+    age: 23,
+    id: 2
+  },
+
+  {
+    name: "kamil",
+    surname: "Nowak",
+    age: 23,
+    id: 3
+  }
+];
+
+class Item extends Comment{
+  render(){
+    const {name, age} = this.props
+    return <div>{name}{age}</div>
   }
 }
-  
-export default App;
+class List extends Component{
+  render(){
+    const myList = this.props.data.map((elem)=>{
+      const {id, ...rest} = elem;
+       return <Item key={id} {...rest}/>
+     });
+     return myList;
+  }
+}
