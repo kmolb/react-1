b46a04c3f930ff7bd03839aa5706fa0bf7417627
Kamil Olber <olb.kamil@gmail.com>
2018-12-01 13:01:46 +0100

    onClick

--- src/App.js
+++ src/App.js
@@ -3,41 +3,14 @@ import React, {
 } from 'react';
 import './App.css';
 
-
-const list = [{
-    name: "Wojtek",
-    surname: "Nowak",
-    age: 23,
-    id: 1
-  },
-
-  {
-    name: "Daniel",
-    surname: "Nowak",
-    age: 23,
-    id: 2
-  },
-
-  {
-    name: "kamil",
-    surname: "Nowak",
-    age: 23,
-    id: 3
+class App extends Component{
+  handleClick(e){
+    console.log(e)
   }
-];
-
-class Item extends Comment{
   render(){
-    const {name, age} = this.props
-    return <div>{name}{age}</div>
+    return <div onClick={this.handleClick}>Element</div>;
   }
 }
-class List extends Component{
-  render(){
-    const myList = this.props.data.map((elem)=>{
-      const {id, ...rest} = elem;
-       return <Item key={id} {...rest}/>
-     });
-     return myList;
-  }
-}
+
+
+export default App;
