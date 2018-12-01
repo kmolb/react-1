6265f243ae626cdc93369fc574e36d1201b76da5
Kamil Olber <olb.kamil@gmail.com>
2018-12-01 12:35:54 +0100

    component 1

--- src/App.js
+++ src/App.js
@@ -1,61 +1,15 @@
 import React, {
-  //Component;
-} from 'react';
+  Component} from 'react';
 import './App.css';
 
-//funkcje function funkcja(){}
-//komponenty function Komponent(){}
+function AppF(props){
+  return <div>Siemaaa</div>
+}
 
-const title = [
-  {
-    lable: 'INFOSHARE 2019',
-    id: 1,
-    expandable: true
-  },{
-    lable: 'CALL FOR SPEAKERS',
-    id: 2,
-    expandable: false
-  },{
-    lable: 'AI ROADSHOW',
-    id: 3,
-    expandable: false
-  },{
-    lable: 'STARTUP CONTEST',
-    id: 4,
-    expandable: false
-  },{
-    lable: 'IS PROJECTS',
-    id: 5,
-    expandable: true
-  },{
-    lable: 'BLOG',
-    id: 6,
-    expandable: false
-  },{
-    lable: 'PREVIUS EDITIONS',
-    id: 7,
-    expandable: true
-  },{
-    lable: 'ABOUT IS',
-    id: 8,
-    expandable: true
+class App extends Component{
+  render (){
+    return <div>Siemaaa</div>;
   }
-]
-
-function Title(props){
-const {lable, expandable} = props.data;
-return <p> {expandable ? ' |#| ' : ''}{lable}</p>
 }
-
-
-function App() {
-  return title.map((title) => {
-    const { id, ...data } = title;
-    return <Title data={data} key={id}/>
-    
-  });
- 
-}
-
   
 export default App;
